c3b7a1d8e52a5fe593abb36c23d7a5869924eced
jiongxuc <joyce.chen@nokia-sbell.com>
2025-01-17 14:23:09 +0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,25 @@ function scrollHandler(event) {
   sections[currentSection].scrollIntoView({ behavior: 'smooth' });
 }
 
+function touchHandler(event) {
+  const touch = event.touches[0];
+  const delta = touch.clientY - startY;
+
+  if (delta < -50 && currentSection < sections.length - 1) {
+    currentSection++;
+  } else if (delta > 50 && currentSection > 0) {
+    currentSection--;
+  }
+
+  sections[currentSection].scrollIntoView({ behavior: 'smooth' });
+}
+
+let startY = 0;
+
 window.addEventListener('wheel', scrollHandler, { passive: false });
+
+window.addEventListener('touchstart', (event) => {
+  startY = event.touches[0].clientY;
+}, { passive: false });
+
+window.addEventListener('touchmove', touchHandler, { passive: false });
